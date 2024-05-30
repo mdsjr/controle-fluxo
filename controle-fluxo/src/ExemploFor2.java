3d8206173bb000852ef6a8d01b46296c8af16615
domingos <mds.junior87@gmail.com>
2024-05-30 20:13:41 -0300

    For em Arrays

---
+++ controle-fluxo/src/ExemploFor2.java
@@ -0,0 +1,10 @@
+public class ExemploFor2 {
+    public static void main(String[] args) {
+        String aluno[] = {"Maria", "Joaquina", "Pedro", "Paulo"};
+
+        for (int x=0; x<aluno.length; x++ ){
+            System.out.println("O aluno do indice " + x + " é " + aluno[x]);
+        }
+    }
+}
+
---
+++ controle-fluxo/src/ExemploFor3.java
@@ -0,0 +1,11 @@
+public class ExemploFor3 {
+    public static void main(String[] args) {
+        String alunos [] = {"Maria","Joana","João","Marcos"};
+
+        for(String aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+    
+}
+
